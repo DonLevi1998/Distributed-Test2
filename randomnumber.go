2c817cdbc598c76f19d07c45832f29c8c605a351
DonLevi1998 <msnarvaez@uce.edu.ec>
2025-05-26 19:20:46 -0500

    second commit

--- randomnumber.go
+++ randomnumber.go
@@ -3,32 +3,19 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 )
 
 func main() {
-	url := "http://www.randomnumberapi.com/api/v1.0/randomnumber"
-
-	// Realiza la solicitud GET
-	resp, err := http.Get(url)
+	resp, err := http.Get("http://www.randomnumberapi.com/api/v1.0/randomnumber")
 	if err != nil {
-		fmt.Println("Error al hacer la solicitud:", err)
-		return
+		panic(err)
 	}
 	defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("Error al leer la respuesta:", err)
-		return
-	}
+	var number []int
+	json.NewDecoder(resp.Body).Decode(&number)
 
+	fmt.Println("Random number:", number[0])
+}
 
-	var numbers []int
-	err = json.Unmarshal(body, &numbers)
-	if err != nil {
-		fmt.Println("Error al parsear JSON:", err)
-		return
-	}
-	fmt.Prin
